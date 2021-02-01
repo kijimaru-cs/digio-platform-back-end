acf20dd94699324699d074be87270fc471b09651
kijimaru01 <suravut2539@hotmail.com>
2021-02-01 16:02:52 +0700

    insert engine

--- index.js
+++ index.js
@@ -225,5 +225,9 @@ app.put("/user/:username", (req, res) => {
     });
   }
 });
+
+app.get("/check", (req, res) => {
+  res.send("ok");
+});
 const port = process.env.PORT || 3001;
 app.listen(port, () => console.log(`Listening on port${port}`));
